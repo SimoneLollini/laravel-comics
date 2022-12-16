44a865e4e38e1005f3c944ee3996212127f54e87
SimoneLollini <lollinisimone@gmail.com>
2022-12-16 19:03:19 +0100

    Add foter social

---
+++ resources/views/partials/footer-social.blade.php
@@ -0,0 +1,77 @@
+<section class="link">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 left">
+                <div class="row">
+                    <div class="col-4">
+                        <div class="wrapper COMICS">
+                            <h6>DC COMICS</h6>
+                            <a href="#">Characters</a>
+                            <a href="#">Comics</a>
+                            <a href="#">Movies</a>
+                            <a href="#">TV</a>
+                            <a href="#">Games</a>
+                            <a href="#">Videos</a>
+                            <a href="#">News</a>
+
+                        </div>
+                        <div class="wrapper SHOP">
+                            <h6>SHOP</h6>
+                            <a href="#">Shop DC</a>
+                            <a href="#">Shop DC Collectibles</a>
+                        </div>
+
+                    </div>
+                    <div class="col-4">
+                        <div class="wrapper DC">
+                            <h6>DC</h6>
+                            <a href="#">Terms Of Use</a>
+                            <a href="#">Privacy policy (New)</a>
+                            <a href="#">Ad Choices</a>
+                            <a href="#">Advertising</a>
+                            <a href="#">Jobs</a>
+                            <a href="#">Subscriptions</a>
+                            <a href="#">Talent Workshops</a>
+                            <a href="#">CPSC Certificates</a>
+                            <a href="#">Ratings</a>
+                            <a href="#">Shop Help</a>
+                            <a href="#">Contact Us</a>
+
+
+                        </div>
+                    </div>
+                    <div class="col-4">
+                        <div class="wrapper SITES">
+                            <h6>SITES</h6>
+                            <a href="#">DC</a>
+                            <a href="#">MAD Magazine</a>
+                            <a href="#">DC Kids</a>
+                            <a href="#">DC Universe</a>
+                            <a href="#">DC Power Visa</a>
+                        </div>
+                    </div>
+                    <div class="col-4">
+                    </div>
+                </div>
+            </div>
+            <div class="col-6 right">
+
+            </div>
+
+        </div>
+    </div>
+</section>
+<section class="social">
+    <div class="container d-flex align-items-center justify-content-between">
+        <a href="#" class="btn btn-outline-primary text-white">SIGN UP NOW!</a>
+        <div class="wrapper_social d-flex align-items-center">
+            <h6 class="text-primary">FOLLOW US</h6>
+            <img src="../../assets/img/footer-facebook.png" alt="">
+            <img src="../../assets/img/footer-twitter.png" alt="">
+            <img src="../../assets/img/footer-youtube.png" alt="">
+            <img src="../../assets/img/footer-pinterest.png" alt="">
+            <img src="../../assets/img/footer-periscope.png" alt="">
+
+        </div>
+    </div>
+</section>
